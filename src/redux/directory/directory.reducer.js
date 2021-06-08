f83d3654a249ade9138f886d30b43926574a74e1
kalina <kalinagercheva@gmail.com>
2021-06-08 13:14:06 +0300

    updates

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.imgur.com/hNuXZ33.jpeg',
+          imageUrl: 'https://images.unsplash.com/photo-1518061124653-4b85d51931f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.imgur.com/T7xUAhW.jpeg',
+          imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.imgur.com/qnczz5A.jpeg',
+          imageUrl: 'https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.imgur.com/cwpEdBk.jpeg',
+          imageUrl: 'https://images.unsplash.com/photo-1575561723050-3a69691d9700?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.imgur.com/CS7Evwj.jpeg',
+          imageUrl: 'https://images.unsplash.com/photo-1517938889432-a2ac9241a486?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -7,55 +7,55 @@ const SHOP_DATA = {
       {
         id: 1,
         name: 'Blue winter beanie',
-        imageUrl: 'https://i.imgur.com/z8V3FdC.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1578019857395-bcfccf0ffb30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         price: 15
       },
       {
         id: 2,
         name: 'White beanie',
-        imageUrl: 'https://i.imgur.com/UBKHdwC.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1514867312438-db0960ee52e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1034&q=80',
         price: 18
       },
       {
         id: 3,
         name: 'Red childrens winter beanie',
-        imageUrl: 'https://i.imgur.com/voSHipn.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1514829006311-076315b13f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         price: 25
       },
       {
         id: 4,
         name: 'Dark grey winter beanie',
-        imageUrl: 'https://i.imgur.com/hSS5f2X.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1549117415-632abd47c19e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         price: 25
       },
       {
         id: 5,
         name: 'White cap',
-        imageUrl: 'https://i.imgur.com/JZbce6Z.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1592367630397-65872fe016e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         price: 16
       },
       {
         id: 6,
         name: 'Beach hat',
-        imageUrl: 'https://i.imgur.com/CrXCm9d.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1531078215167-91fcfe45b39e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1098&q=80',
         price: 14
       },
       {
         id: 7,
         name: 'Yellow cap',
-        imageUrl: 'https://i.imgur.com/cb2aiTN.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1575428652377-a2d80e2277fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
         price: 12
       },
       {
         id: 8,
         name: 'White summer hat',
-        imageUrl: 'https://i.imgur.com/iA3zSmw.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1543415023-96049d88764b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
         price: 14
       },
       {
         id: 9,
         name: 'Black stylish hat',
-        imageUrl: 'https://i.imgur.com/mKSsV0T.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1565839412225-98641b2d7b5b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         price: 24
       }
     ]
@@ -68,49 +68,49 @@ const SHOP_DATA = {
       {
         id: 10,
         name: 'Durable sneakers for running',
-        imageUrl: 'https://i.imgur.com/8CoEQhk.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1566824837278-216c7d60d391?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         price: 150
       },
       {
         id: 11,
         name: 'Black casual sneakers',
-        imageUrl: 'https://i.imgur.com/IooSejW.jpeg',
+        imageUrl: 'https://i.imgur.com/V5NBkMh.png',
         price: 275
       },
       {
         id: 12,
         name: 'Colorful casual sneakers',
-        imageUrl: 'https://i.imgur.com/iGbS2Qj.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
         price: 110
       },
       {
         id: 13,
         name: 'Nike golden edition sneakers',
-        imageUrl: 'https://i.imgur.com/enXBIKt.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1586614340325-da2b80e3812a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         price: 260
       },
       {
         id: 14,
         name: 'Light-pink casual sneakers',
-        imageUrl: 'https://i.imgur.com/FBFkeC6.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1499013819532-e4ff41b00669?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         price: 160
       },
       {
         id: 15,
         name: 'Nike white casual sneakers',
-        imageUrl: 'https://i.imgur.com/niuhdri.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1596480370804-cff0eed14888?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=761&q=80',
         price: 180
       },
       {
         id: 16,
         name: 'black & blue casual sneakers',
-        imageUrl: 'https://i.imgur.com/hMlEk2r.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1566652655078-5903d1da7000?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         price: 120
       },
       {
         id: 17,
         name: 'Waterproof green sneakers',
-        imageUrl: 'https://i.imgur.com/Pg3TVwn.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1106&q=80',
         price: 200
       }
     ]
@@ -123,31 +123,31 @@ const SHOP_DATA = {
       {
         id: 18,
         name: 'Black stylish leather jacket',
-        imageUrl: 'https://i.imgur.com/oYuzQrL.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1520975916090-3105956dac38?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 175
       },
       {
         id: 19,
         name: 'Light-blue jean jacket',
-        imageUrl: 'https://i.imgur.com/6PXN071.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1570730918769-76e0d144cdc4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 90
       },
       {
         id: 20,
         name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.imgur.com/tP4J5d0.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1577660002965-04865592fc60?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         price: 90
       },
       {
         id: 21,
         name: 'Brown jacket',
-        imageUrl: 'https://i.imgur.com/9RAk0J3.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
         price: 75
       },
       {
         id: 22,
         name: 'Mixed teen jacket',
-        imageUrl: 'https://i.imgur.com/CfqOK3V.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1603205954864-97a96582eb7f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 50
       }
     ]
@@ -160,43 +160,43 @@ const SHOP_DATA = {
       {
         id: 23,
         name: 'Light-pink T-shirt',
-        imageUrl: 'https://i.imgur.com/obEXMd3.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1580991584164-a4e12c31521d?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         price: 30
       },
       {
         id: 24,
         name: 'White tank top',
-        imageUrl: 'https://i.imgur.com/C7dE448.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1544383014-93d13ebcd2da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
         price: 25
       },
       {
         id: 25,
         name: 'Yellow track suit',
-        imageUrl: 'https://i.imgur.com/7y0n2GT.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=662&q=80',
         price: 120
       },
       {
         id: 26,
         name: 'White suit',
-        imageUrl: 'https://i.imgur.com/1nDpjqi.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1587003484867-ba695bd4e4ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 150
       },
       {
         id: 27,
-        name: 'White spotted dress',
-        imageUrl: 'https://i.imgur.com/xEPy3xe.jpeg',
+        name: 'Light-blue beach dress',
+        imageUrl: 'https://images.unsplash.com/photo-1539008835657-9e8e9680c956?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 45
       },
       {
         id: 28,
         name: 'Blue yoga pants',
-        imageUrl: 'https://i.imgur.com/VKpGDB4.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         price: 35
       },
       {
         id: 29,
         name: 'Red sleeveless dress',
-        imageUrl: 'https://i.imgur.com/BA55d86.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1572804013854-26c537be296e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
         price: 20
       }
     ]
@@ -209,19 +209,19 @@ const SHOP_DATA = {
       {
         id: 30,
         name: 'White blouse',
-        imageUrl: 'https://i.imgur.com/NOYl5Cv.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1603711913080-98744ea161f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
         price: 30
       },
       {
         id: 31,
         name: 'Denim hoodie',
-        imageUrl: 'https://i.imgur.com/rxhojuk.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1566802842272-e694af42eb29?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80',
         price: 50
       },
       {
         id: 32,
         name: 'Basic white T-shirt',
-        imageUrl: 'https://i.imgur.com/vdhRcYg.jpeg',
+        imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         price: 15
       },
       {
